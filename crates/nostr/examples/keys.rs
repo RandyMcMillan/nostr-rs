223f07787149a5f35722130ff5f8635437f599bc
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-11-30 08:26:59 -0500

    diff --git a/crates/nostr/examples/keys.rs b/crates/nostr/examples/keys.rs index a65547a1..b7bfe46f 100644 --- a/crates/nostr/examples/keys.rs +++ b/crates/nostr/examples/keys.rs @@ -8,21 +8,29 @@ use std::path::PathBuf;
     use git2::{Error, Repository};
     use nostr::prelude::*;
    +use bitcoin::key::Parity;
    
     fn walk() -> Result<()> {
         let mut mtimes: HashMap<PathBuf, i64> = HashMap::new();
         let repo = Repository::open(".")?;
         let mut revwalk = repo.revwalk()?;
    +    let mut parent_privkey =
    +        Keys::parse("0000000000000000000000000000000000000000000000000000000000000001")?;
    +    let mut parent_pubkey = parent_privkey.public_key();
         revwalk.set_sorting(git2::Sort::TIME)?;
         revwalk.push_head()?;
         for commit_id in revwalk {
    +        println!("");
    +        println!("parent_privkey hash:{:?}", parent_privkey);
    +        println!("");
    +        println!("parent_pubkey  hash:{}", parent_pubkey.public_key(Parity::Even));
             let commit_id = commit_id?;
             let commit = repo.find_commit(commit_id)?;
    -        //println!("{:0>64?}", commit.id());
             let pk = format!("{:0>64?}", commit.id());
    -        println!("padded commit hash:{}", pk);
    -        let keys = Keys::parse(pk);
    -        println!("pubkey from   hash:{}", keys?.public_key());
    +        println!("padded commit  hash:{}", pk);
    +        let keys = Keys::parse(pk.clone())?.clone();
    +        println!("pubkey from    hash:{}", keys.public_key());
    +        parent_privkey = Keys::parse(pk)?;
         }
         Ok(())
     }
    @@ -33,10 +41,10 @@ fn main() -> Result<()> {
         let public_key = keys.public_key();
         let secret_key = keys.secret_key();
    
    -    println!("Public key: {}", public_key);
    -    println!("Public key bech32: {}", public_key.to_bech32()?);
    -    println!("Secret key: {}", secret_key.to_secret_hex());
    -    println!("Secret key bech32: {}", secret_key.to_bech32()?);
    +    //println!("Public key: {}", public_key);
    +    //println!("Public key bech32: {}", public_key.to_bech32()?);
    +    //println!("Secret key: {}", secret_key.to_secret_hex());
    +    //println!("Secret key bech32: {}", secret_key.to_bech32()?);
    
         // Bech32 keys
         let secret_key_from_bech32 =
    @@ -46,12 +54,12 @@ fn main() -> Result<()> {
         let secret_key =
             SecretKey::parse("nsec1j4c6269y9w0q2er2xjw8sv2ehyrtfxq3jwgdlxj6qfn8z4gjsq5qfvfk99")?;
         let keys = Keys::new(secret_key);
    -    println!("parse:Public key: {}", keys.public_key());
    +    //println!("parse:Public key: {}", keys.public_key());
         let keys = Keys::parse("0000000000000000000000000000000000000000000000000000000000000001")?;
    -    println!("01:Public key: {}", keys.public_key());
    +    //println!("01:Public key: {}", keys.public_key());
         let keys = Keys::parse("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")?;
    
    -    println!("e3:Public key: {}", keys.public_key());
    -    walk();
    +    //println!("e3:Public key: {}", keys.public_key());
    +    let _ = walk();
         Ok(())
     }

--- crates/nostr/examples/keys.rs
+++ crates/nostr/examples/keys.rs
@@ -8,21 +8,29 @@ use std::path::PathBuf;
 
 use git2::{Error, Repository};
 use nostr::prelude::*;
+use bitcoin::key::Parity;
 
 fn walk() -> Result<()> {
     let mut mtimes: HashMap<PathBuf, i64> = HashMap::new();
     let repo = Repository::open(".")?;
     let mut revwalk = repo.revwalk()?;
+    let mut parent_privkey =
+        Keys::parse("0000000000000000000000000000000000000000000000000000000000000001")?;
+    let mut parent_pubkey = parent_privkey.public_key();
     revwalk.set_sorting(git2::Sort::TIME)?;
     revwalk.push_head()?;
     for commit_id in revwalk {
+        println!("");
+        println!("parent_privkey hash:{:?}", parent_privkey);
+        println!("");
+        println!("parent_pubkey  hash:{}", parent_pubkey.public_key(Parity::Even));
         let commit_id = commit_id?;
         let commit = repo.find_commit(commit_id)?;
-        //println!("{:0>64?}", commit.id());
         let pk = format!("{:0>64?}", commit.id());
-        println!("padded commit hash:{}", pk);
-        let keys = Keys::parse(pk);
-        println!("pubkey from   hash:{}", keys?.public_key());
+        println!("padded commit  hash:{}", pk);
+        let keys = Keys::parse(pk.clone())?.clone();
+        println!("pubkey from    hash:{}", keys.public_key());
+        parent_privkey = Keys::parse(pk)?;
     }
     Ok(())
 }
@@ -33,10 +41,10 @@ fn main() -> Result<()> {
     let public_key = keys.public_key();
     let secret_key = keys.secret_key();
 
-    println!("Public key: {}", public_key);
-    println!("Public key bech32: {}", public_key.to_bech32()?);
-    println!("Secret key: {}", secret_key.to_secret_hex());
-    println!("Secret key bech32: {}", secret_key.to_bech32()?);
+    //println!("Public key: {}", public_key);
+    //println!("Public key bech32: {}", public_key.to_bech32()?);
+    //println!("Secret key: {}", secret_key.to_secret_hex());
+    //println!("Secret key bech32: {}", secret_key.to_bech32()?);
 
     // Bech32 keys
     let secret_key_from_bech32 =
@@ -46,12 +54,12 @@ fn main() -> Result<()> {
     let secret_key =
         SecretKey::parse("nsec1j4c6269y9w0q2er2xjw8sv2ehyrtfxq3jwgdlxj6qfn8z4gjsq5qfvfk99")?;
     let keys = Keys::new(secret_key);
-    println!("parse:Public key: {}", keys.public_key());
+    //println!("parse:Public key: {}", keys.public_key());
     let keys = Keys::parse("0000000000000000000000000000000000000000000000000000000000000001")?;
-    println!("01:Public key: {}", keys.public_key());
+    //println!("01:Public key: {}", keys.public_key());
     let keys = Keys::parse("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")?;
 
-    println!("e3:Public key: {}", keys.public_key());
-    walk();
+    //println!("e3:Public key: {}", keys.public_key());
+    let _ = walk();
     Ok(())
 }
